f549cb0be3f5147845f2938283434d602b2b1551
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2024-11-29 19:37:42 +0200

    fix: updated App for routes

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,30 +1,30 @@
 import './App.css'
-//import Login from './pages/Login'
-//import { GiftIcon } from '@heroicons/react/20/solid';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Suspense } from 'react';
+
 import ProtectedRoute from '@/router/ProtectedRoute';
 import routes from '@/router/routes';
+
 import Login from '@/pages/Login';
 import SignUp from '@/pages/SignUp';
 import AuthLayout from '@/layouts/auth-layout/AuthLayout';
 import MainLayout from '@/layouts/main-layout/MainLayout';
+import Loader from '@/components/loader/Loader';
 
 function App() {
   const isAuthenticated = true;
   
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
-          {/* Публічні маршрути */}
           <Route element={<AuthLayout />}>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
           </Route>
           
 
-          {/* Захищені маршрути */}
           {routes.map((route) =>
             route.path === '/' ? (
               <Route
